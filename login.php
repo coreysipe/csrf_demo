3f83367d2b97162d1fde00c7d5c98b6c55a4dd26
Corey Sipe <113368479+coreysipe@users.noreply.github.com>
2023-11-26 17:01:56 -0500

    Update login.php

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset(
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
